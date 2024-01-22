94e50428c0f83767e46432ea1cfbc615767960e2
Rui Vieira <revs87@msn.com>
2024-01-22 01:42:53 +0000

    AuthResponse code to Int

--- src/main/kotlin/pt/rvcoding/domain/AuthRepository.kt
+++ src/main/kotlin/pt/rvcoding/domain/AuthRepository.kt
@@ -6,22 +6,22 @@ interface AuthRepository {
     fun changePassword(username: String?, password: String?, newPassword: String?): ChangePasswordResult
 }
 
-sealed class RegisterResult(val code: String) {
-    data object Success : RegisterResult("100")
-    data object InvalidParametersError : RegisterResult("101")
-    data object CriteriaNotMetError : RegisterResult("102")
-    data object UserAlreadyRegisteredError : RegisterResult("103")
+sealed class RegisterResult(val code: Int) {
+    data object Success : RegisterResult(100)
+    data object InvalidParametersError : RegisterResult(101)
+    data object CriteriaNotMetError : RegisterResult(102)
+    data object UserAlreadyRegisteredError : RegisterResult(103)
 }
 
-sealed class LoginResult(val code: String) {
-    data object Success : LoginResult("200")
-    data object InvalidParametersError : LoginResult("201")
-    data object CredentialsMismatchError : LoginResult("202")
+sealed class LoginResult(val code: Int) {
+    data object Success : LoginResult(200)
+    data object InvalidParametersError : LoginResult(201)
+    data object CredentialsMismatchError : LoginResult(202)
 }
 
-sealed class ChangePasswordResult(val code: String) {
-    data object Success : ChangePasswordResult("300")
-    data object InvalidParametersError : ChangePasswordResult("301")
-    data object CredentialsMismatchError : ChangePasswordResult("302")
-    data object CriteriaNotMetError : ChangePasswordResult("303")
+sealed class ChangePasswordResult(val code: Int) {
+    data object Success : ChangePasswordResult(300)
+    data object InvalidParametersError : ChangePasswordResult(301)
+    data object CredentialsMismatchError : ChangePasswordResult(302)
+    data object CriteriaNotMetError : ChangePasswordResult(303)
 }
--- src/main/kotlin/pt/rvcoding/domain/models/AuthResponse.kt
+++ src/main/kotlin/pt/rvcoding/domain/models/AuthResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AuthResponse(
-    val code: String,
+    val code: Int,
     val type: String,
     val message: String
 )
