04c15d7794fad5006f788604e173643b24433b7e
Rui Vieira <revs87@msn.com>
2024-04-04 21:49:41 +0100

    Unknown endpoint test added

--- src/test/kotlin/pt/rvcoding/plugins/RoutingRootTest.kt
+++ src/test/kotlin/pt/rvcoding/plugins/RoutingRootTest.kt
@@ -4,17 +4,34 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.koin.core.context.GlobalContext.stopKoin
 import pt.rvcoding.domain.Route
+import pt.rvcoding.domain.response.AuthResponse
 
 class RoutingRootTest {
 
+    @AfterEach
+    fun tearDown() { stopKoin() }
+
     @Test
-    fun `testRoot GIVEN root endpoint THEN returns Hello message`() = testApplication {
+    fun `access root endpoint, returns Hello message`() = testApplication {
         val response = client.get(Route.Root.path)
         assertEquals(HttpStatusCode.OK, response.status)
         assertEquals("Hello, Kotlin Multiplatform guinea pig!", response.bodyAsText())
+    }
+
+    @Test
+    fun `access unknown endpoint, returns Not found message`() = testApplication {
+        val response = client.get("/unknown")
+        assertEquals(HttpStatusCode.NotFound, response.status)
+        val actual = Json.decodeFromString<AuthResponse>(response.bodyAsText())
+        assertEquals(Error.NotFoundError.code, actual.code)
+        assertEquals("NotFoundError", actual.type)
+        assertEquals("Not found.", actual.message)
     }
 
 }
