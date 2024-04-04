7df0c9185320c1e75b814e2e71e3d77caa5c578d
Rui Vieira <revs87@msn.com>
2024-04-04 20:30:39 +0100

    Using Route.Hello.path value

--- src/test/kotlin/pt/rvcoding/plugins/RoutingRootTest.kt
+++ src/test/kotlin/pt/rvcoding/plugins/RoutingRootTest.kt
@@ -6,12 +6,13 @@ import io.ktor.http.*
 import io.ktor.server.testing.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import pt.rvcoding.domain.Route
 
 class RoutingRootTest {
 
     @Test
     fun `testRoot GIVEN root endpoint THEN returns Hello message`() = testApplication {
-        val response = client.get("/")
+        val response = client.get(Route.Hello.path)
         assertEquals(HttpStatusCode.OK, response.status)
         assertEquals("Hello, Kotlin Multiplatform guinea pig!", response.bodyAsText())
     }
